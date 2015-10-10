bd6ea14c4f2d3abcd609dde247f18280e935ef81
martinaas <maregu@gmail.com>
2015-10-10 08:14:01 +0100

    Changed to add targetVertexId

--- src/ShortestPathsComputation.java
+++ src/ShortestPathsComputation.java
@@ -42,7 +42,7 @@ public class ShortestPathsComputation extends BasicComputation<
       if (minDist < vertex.getValue().get()) {
           vertex.setValue(new IntWritable(minDist));
           for (Edge<IntWritable, NullWritable> edge : vertex.getEdges()) {
-              int distance = minDist;
+              int distance = minDist + edge.getTargetVertexId().get();
               sendMessage(edge.getTargetVertexId(), new IntWritable(distance));
           }
       }
